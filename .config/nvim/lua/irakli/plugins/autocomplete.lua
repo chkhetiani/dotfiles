9e6eacbdd5cc510023d8eb7ce5197654f684a7ef
irakli.chkhetiani <chkhetiani.i@gmail.com>
2024-06-04 18:31:40 +0400

    added lua dev plugin

--- .config/nvim/lua/irakli/plugins/autocomplete.lua
+++ .config/nvim/lua/irakli/plugins/autocomplete.lua
@@ -68,4 +68,11 @@ return {
             },
         }
     end,
+    opts = function (_, opts)
+        opts.sources = opts.sources or {}
+        table.insert(opts.sources, {
+            name = "lazydev",
+            group_index = 0,
+        })
+    end,
 }
