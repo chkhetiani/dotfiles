2f4c6fbc355ba63f0d690afae0f3ee5e3d638690
irakli.chkhetiani <chkhetiani.i@gmail.com>
2024-06-13 11:03:40 +0400

    add back format keymap

--- .config/nvim/lua/irakli/plugins/lsp.lua
+++ .config/nvim/lua/irakli/plugins/lsp.lua
@@ -21,6 +21,7 @@ return {
                 map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
                 map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
                 map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+                map('<leader>f', vim.lsp.buf.format, '[F]ormat')
 
                 map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
                 map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
