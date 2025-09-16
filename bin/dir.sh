7d53c495dab13f8447bb9d58d86f25dcdb4735a4
irakli.chkhetiani <chkhetiani.i@gmail.com>
2025-09-16 17:29:53 +0400

    on selecting empty string open home dir

--- bin/dir.sh
+++ bin/dir.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-#
-selected=$(find /home/irakli -not -path '*/.*' -type d -printf '%P\n' | rofi -dmenu -sorting-method fzf -sort -i -p "dir")
+selected=$(
+    find /home/irakli -not -path '*/.*' -type d -printf '%P\n' |
+    rofi -dmenu -sorting-method fzf -sort -i -p "dir"
+)
+rofi_exit=$?
 
-if [ -n "$selected" ]; then
+if [[ $rofi_exit -ne 0 ]]; then
+    exit 0
+fi
+
+if [[ -z "$selected" ]]; then
+    thunar "/home/irakli"
+else
     thunar "/home/irakli/$selected"
 fi
